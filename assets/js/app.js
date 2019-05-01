67e8aa893831fd49f53b34823a4fd04683013073
Fabien Mansoubi <fabienmp@gmail.com>
2019-05-02 00:58:25 +0200

    Added geolocation api from google to locate ZIPCODE and AUTOCOMPLETE

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,49 @@
 $(document).ready(function () {
 
+
     var ATTOM_API_KEY = '736f1130096aa92549d800921bca8e8c';
     var ZILLOW_API_KEY = 'X1-ZWz1h1dha4cc97_68qz7';
+    var GOOGLE_API_KEY = 'AIzaSyDUXdBkAnO21_I-Rp16IlMg6JDpVOsFF8s';
 
     var parseXml;
 
-  //Variables for user input.
+    //Variables for user input.
     //5 digit integer zip code.
-        var zipCode1 = 0;
-        var zipCode2 = 0;
+    var zipCode1 = 0;
+    var zipCode2 = 0;
 
     //Graph 1: Reading Analysis
-        //Cost associated with magazing and newspaper subscriptions, nonsubscriptions and books. National average is 100.
-            //(response.response.result.package.item[0].expread);
-            var zipCode1_expread = 0;
-            var zipCode2_expread = 0;
+    //Cost associated with magazing and newspaper subscriptions, nonsubscriptions and books. National average is 100.
+    //(response.response.result.package.item[0].expread);
+    var zipCode1_expread = 0;
+    var zipCode2_expread = 0;
 
     //Graph 2: Foreclosure and Crime Comparison
-        //Total crime risk. Average national value is 100.
-        //(response.response.result.package.item[0].cocrmcytotc);
-            var zipCode1_cocrmcytotc = 0;
-            var zipCode2_cocrmcytotc = 0;
-        //Total number of dwellings vacant in the area.
-        //(response.response.result.package.item[0].dwlvacnt);
-            var zipCode1_dwlvacnt = 0;
-            var zipCode2_dwlvacnt = 0;
+    //Total crime risk. Average national value is 100.
+    //(response.response.result.package.item[0].cocrmcytotc);
+    var zipCode1_cocrmcytotc = 0;
+    var zipCode2_cocrmcytotc = 0;
+    //Total number of dwellings vacant in the area.
+    //(response.response.result.package.item[0].dwlvacnt);
+    var zipCode1_dwlvacnt = 0;
+    var zipCode2_dwlvacnt = 0;
 
     //Below is a scalable implementation that we may decide to use later on. For now we are going to use a hardcoded variable schema.
-            // //First zip code, first paramater value and field name from API.
-            // var zipCode1_param1_value;
-            // var zipCode1_param1_fieldname;
+    // //First zip code, first paramater value and field name from API.
+    // var zipCode1_param1_value;
+    // var zipCode1_param1_fieldname;
 
-            // //First zip code, second paramater value and field name from API.
-            // var zipCode1_param2_value;
-            // var zipCode1_param2_fieldname;
+    // //First zip code, second paramater value and field name from API.
+    // var zipCode1_param2_value;
+    // var zipCode1_param2_fieldname;
 
-            // //Second zip code, first paramater value and field name from API.
-            // var zipCode2_param1_value;
-            // var zipCode2_param1_fieldname;
+    // //Second zip code, first paramater value and field name from API.
+    // var zipCode2_param1_value;
+    // var zipCode2_param1_fieldname;
 
-            // //Second zip code, second paramater value and field name from API.
-            // var zipCode2_param2_value;
-            // var zipCode2_param2_fieldname;
+    // //Second zip code, second paramater value and field name from API.
+    // var zipCode2_param2_value;
+    // var zipCode2_param2_fieldname;
 
 
     if (typeof window.DOMParser != "undefined") {
@@ -149,9 +151,8 @@ $(document).ready(function () {
 
         //var zipCode1 = 94040;
         //var url = 'https://search.onboard-apis.com/communityapi/v2.0.0/attribute/lookup';
-        var url = ("https://search.onboard-apis.com/communityapi/v2.0.0/Area/Full/?AreaId=ZI"+ zipCode1);
-        var data = {
-        }
+        var url = ("https://search.onboard-apis.com/communityapi/v2.0.0/Area/Full/?AreaId=ZI" + zipCode1);
+        var data = {}
         var headers = {
             'Accept': 'application/json',
             'apikey': '736f1130096aa92549d800921bca8e8c'
@@ -159,11 +160,11 @@ $(document).ready(function () {
 
         function responseHandler(response) {
             var printedText = JSON.stringify(response, undefined, 4);
-            
+
             //TESTING CONSOLE
-                //console.log(response);
-                //console.log(response.response.result.package.item[0].popdnsty);
-                
+            //console.log(response);
+            //console.log(response.response.result.package.item[0].popdnsty);
+
             document.getElementById("APIreturn").value = printedText;
             document.getElementById('ATTOM_COMMUNITY_ATTRIBUTES__textarea').value = printedText;
         }
@@ -171,5 +172,15 @@ $(document).ready(function () {
         makeAjaxGetCall(url, headers, data, responseHandler);
 
     });
-
 });
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+
---
+++ assets/js/geolocation.js
@@ -0,0 +1,179 @@
+////  Global Vars
+var editClntInfoAutocompleteZipCode1, editClntInfoAutocompleteZipCode2;
+var marker1, marker2;
+var zipCode1Map, zipCode2Map;
+var addrStreet = "",
+    addressComponets = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+    };
+
+var options = {
+    types: ['(regions)'],
+    componentRestrictions: {
+        country: "us"
+    }
+};
+
+function initEditClntInfoAutoComplete() {
+
+    editClntInfoAutocompleteZipCode1 = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */
+        (document.getElementById('zipCode1Input')), options);
+
+    zipCode1Map = new google.maps.Map(document.getElementById('mapZipCode1'), {
+        center: {
+            lat: -33.8688,
+            lng: 151.2195
+        },
+        zoom: 10
+    });
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    editClntInfoAutocompleteZipCode1.bindTo('bounds', zipCode1Map);
+    marker1 = new google.maps.Marker({
+        map: zipCode1Map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    editClntInfoAutocompleteZipCode2 = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */
+        (document.getElementById('zipCode2Input')), options);
+
+    zipCode2Map = new google.maps.Map(document.getElementById('mapZipCode2'), {
+        center: {
+            lat: -33.8688,
+            lng: 151.2195
+        },
+        zoom: 10
+    });
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    editClntInfoAutocompleteZipCode2.bindTo('bounds', zipCode2Map);
+    marker2 = new google.maps.Marker({
+        map: zipCode2Map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    editClntInfoAutocompleteZipCode1.addListener('place_changed', fillInEditClntInfoAddress1);
+    editClntInfoAutocompleteZipCode2.addListener('place_changed', fillInEditClntInfoAddress2);
+}
+
+function fillInEditClntInfoAddress1() {
+
+    marker1.setVisible(false);
+
+    var place = editClntInfoAutocompleteZipCode1.getPlace();
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        zipCode1Map.fitBounds(place.geometry.viewport);
+    } else {
+        zipCode1Map.setCenter(place.geometry.location);
+    }
+
+    marker1.setPosition(place.geometry.location);
+    marker1.setVisible(true);
+    zipCode1Map.setZoom(12);
+
+    for (var i = 0; i < place.address_components.length; i++) {
+
+        var addressType = place.address_components[i].types[0];
+        if (addressComponets[addressType]) {
+            var val = place.address_components[i][addressComponets[addressType]];
+
+            assignEditFrmAddrFieldsVal1(addressType, val);
+        }
+
+    }
+
+    //if (addrStreet != "")
+    //document.getElementById("zipCode1Input").value = addrStreet;
+
+}
+
+function fillInEditClntInfoAddress2() {
+    
+    marker2.setVisible(false);
+
+    var place = editClntInfoAutocompleteZipCode2.getPlace();
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        zipCode2Map.fitBounds(place.geometry.viewport);
+    } else {
+        zipCode2Map.setCenter(place.geometry.location);
+    }
+
+    marker2.setPosition(place.geometry.location);
+    marker2.setVisible(true);
+    zipCode2Map.setZoom(12); 
+
+    for (var i = 0; i < place.address_components.length; i++) {
+
+        var addressType = place.address_components[i].types[0];
+        if (addressComponets[addressType]) {
+            var val = place.address_components[i][addressComponets[addressType]];
+            assignEditFrmAddrFieldsVal2(addressType, val);
+        }
+
+    }
+
+    //if (addrStreet != "")
+    //document.getElementById("zipCode2Input").value = addrStreet;
+
+}
+
+function assignEditFrmAddrFieldsVal1(addressType, val) {
+
+    switch (addressType) {
+        case "administrative_area_level_1":
+            //document.getElementById("").value = val;
+            break;
+        case "locality":
+            //document.getElementById("").value = val;
+            break;
+        case "country":
+            //document.getElementById("").value = val;
+            break;
+        case "postal_code":
+            document.getElementById("zipCode1Input").value = val;
+            break;
+        case "street_number":
+        case "route":
+            addrStreet += " " + val;
+            break;
+    }
+}
+
+function assignEditFrmAddrFieldsVal2(addressType, val) {
+
+    switch (addressType) {
+        case "administrative_area_level_1":
+            //document.getElementById("").value = val;
+            break;
+        case "locality":
+            //document.getElementById("").value = val;
+            break;
+        case "country":
+            //document.getElementById("").value = val;
+            break;
+        case "postal_code":
+            document.getElementById("zipCode2Input").value = val;
+            break;
+        case "street_number":
+        case "route":
+            addrStreet += " " + val;
+            break;
+    }
+}
